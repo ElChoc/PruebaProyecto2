4cc17435aa2cd85d87be4416cb2a93be12b29620
chocl <chocl@G3>
2022-09-20 09:17:29 -0500

    Agregar archivos de proyecto.

---
+++ PruebaProyecto2/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project2
+{
+    internal class Vehicle
+    {
+        public int year { get; set; }
+        public string brand { get; set; }
+        public string description { get; set; }
+        public string wheels { get; set; }
+        public string color { get; set; }
+        //public License license { get; set; }
+        public string type { get; set; }
+
+    }
+}
